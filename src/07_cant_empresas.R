22e3f7aff81e75a255e72dce5934be921554f31e
gefero <german.rosati@gmail.com>
2026-06-29 11:58:13 -0300

    Agrego maqueta indicador 07

--- src/07_cant_empresas.R
+++ src/07_cant_empresas.R
@@ -3,12 +3,14 @@ source('./style/fundar_monitor_theme.R')
 
 df <- read_csv("./data/inputs_md/07_serie_empresas_por_jurisdiccion.csv")
 
+noa <- c("Catamarca", "Jujuy", "Salta","Santiago del Estero", "Tucumán", "La Rioja")
+
 df <- df %>%
-  mutate(la_rioja_aglo = if_else(jurisdiccion == "La Rioja", "La Rioja", "Resto"),
+  mutate(
+    noa_resto = if_else(jurisdiccion %in% noa, "NOA", "Resto"),
          la_rioja_region = case_when(
            jurisdiccion == "La Rioja" ~ "3. La Rioja", 
-           jurisdiccion %in% 
-             c("Catamarca", "Jujuy", "Salta","Santiago del Estero", "Tucumán") ~ "2. NOA-Resto",
+           jurisdiccion %in% noa & jurisdiccion != "La Rioja" ~ "2. NOA-Resto",
            TRUE ~ "1. Resto país"))
 
 df %>%
@@ -20,12 +22,13 @@ df %>%
   geom_line(linewidth = 0.7) +
   scale_color_fundar_multi(name = "Jurisdicción") +
   theme_monitor() +
-  scale_x_date(date_labels = "%m-%Y", date_breaks = "1 month") +
+  scale_x_date(date_labels = "%m-%Y", date_breaks = "4 month") +
   labs(
     title   = "Cantidad de empresas por jurisdicción",
     x       = "Fecha",
     y       = "Cantidad de empresas",
     caption = fuente_fundar("Fundar, con base en datos de SRT.")
-  )
+  ) + 
+  facet_wrap(~noa_resto, scales = "free_y")
 
 ggsave('./outputs/plots/11_empresas_jurisdiccion.png', width = 12, height = 7)
